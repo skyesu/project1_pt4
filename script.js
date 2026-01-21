780202e1460a3b6038bc07327ea281d816dc0d24
skyesu <sbs.garcia2004@gmail.com>
2026-01-21 00:26:38 -0500

    final_touches

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function pauseMusic() {
 function playMusic() {
     bgm.play();
 }
+
+let modalBtns = document.getElementsByClassName("modal-btn");
+let closeBtns = document.getElementsByClassName("close");
+
+for(let modalBtn of modalBtns) {
+modalBtn.onclick = function(event) {
+    document.querySelector(event.target.getAttribute('href')) = "block";
+}
+}
+
+for(let closeBtn of closeBtns) {
+closeBtn.onclick = function(event) {
+        event.target.parentNode.parentNode.style.display = 'none';
+}
+}
+
+window.onkeydown = function(event) {
+    if (event.key == 'Escape') {
+        for(let modal of modals) {
+            modal.style.display = 'none';
+        }
+    }
+}
